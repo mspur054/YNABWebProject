e95dbe5c5ce71ad803ba35f8011e495bb3e67b95
Matt <mspur054@uottawa.ca>
2018-08-27 20:48:25 -0400

    added project

--- YNABWebProject/YNABWebProject/my_script.py
+++ YNABWebProject/YNABWebProject/my_script.py
@@ -1,3 +1,10 @@
 from passwords import YNAB_KEY
+import json
+import requests, sys, socket
+
 
 KEY = YNAB_KEY
+BASE_URL = 'https://api.youneedabudget.com/v1'
+
+r = requests.get(BASE_URL + '/user', headers = {'Authorization': YNAB_KEY}
+
